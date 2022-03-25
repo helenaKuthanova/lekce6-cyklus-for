f4beef4977092759fcb175647a4f04ef0df96fe1
helenaKuthanova <helena.kuthanova@gmail.com>
2022-03-25 14:21:14 +0100

    úkol s použitím cyklu while

--- main.js
+++ main.js
@@ -6,12 +6,14 @@
 console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
-for (let i=0; i<11; i++) {
-    console.log(i)
+let a = 0;
+
+while (a<11) {
+    console.log(a);
+    a++;
 }
 
 console.log('-------------------');
-
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -20,25 +22,34 @@ console.log('-------------------');
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
-for (let i=0; i<11; i++) {
-    console.log(i)
+b = 0;
+
+while (b<11) {
+    console.log(b);
+    b++;
 }
+
 console.log("-")
 
-for (let i=0; i<=11; i++) {
-    console.log(i)
+c = 0; 
+
+while (c<=11) {  // výpis včetně 11
+    console.log(c);
+    c++;
 }
 
 console.log('-------------------');
-
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-for (let i=10; i>=0; i--) {
-    console.log(i);
+d = 10;
+
+while (d>=0) {
+    console.log(d);
+    d--;
 }
 
 console.log('-------------------');
